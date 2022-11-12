6d992a0cc4a2681b8a5dd51043374e6d2c077d43
Syahda Fahreza <syahdafahreza@gmail.com>
2022-11-12 09:45:42 +0700

    Update whatsapp-onlineswitch.js

--- js/whatsapp-onlineswitch.js
+++ js/whatsapp-onlineswitch.js
@@ -1,30 +1,13 @@
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const d = new Date();
 let day = weekday[d.getDay()];
-if (day == "Monday") {
-	if (new Date().getHours() >= 8 && < 16 ) {
-  document.getElementById("OnlineStatus").innerHTML = "Online";}
+
+if (day != "Sunday" && day != "Saturday" && new Date().getHours() >= 8 && new Date().getHours() < 16 ) {
+	
+  document.getElementById("demo").innerHTML = "Online";
 }
-else if (day == "Tuesday") {
-	if (new Date().getHours() >= 8 && < 16 ) {
-  document.getElementById("OnlineStatus").innerHTML = "Online";}
-}
-else if (day == "Wednesday") {
-	if (new Date().getHours() >= 8 && < 16 ) {
-  document.getElementById("OnlineStatus").innerHTML = "Online";}
-}
-else if (day == "Thursday") {
-	if (new Date().getHours() >= 8 && < 16 ) {
-  document.getElementById("OnlineStatus").innerHTML = "Online";}
-}
-else if (day == "Friday") {
-	if (new Date().getHours() >= 8 && < 16 ) {
-  document.getElementById("OnlineStatus").innerHTML = "Online";}
-}
-else if (day == "Saturday") {
-	if (new Date().getHours() >= 8 && < 1 ) {
-  document.getElementById("OnlineStatus").innerHTML = "Online";}
-}
-else {
-  document.getElementById("OnlineStatus").innerHTML = "Offline";
+else if (day == "Saturday" && new Date().getHours() >= 8 && new Date().getHours() < 13) {
+  document.getElementById("demo").innerHTML = "Online";
+}else {
+  document.getElementById("demo").innerHTML = "Offline";
 }
