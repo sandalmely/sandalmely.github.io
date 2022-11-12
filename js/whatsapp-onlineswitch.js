52b551f86aebd0790184faffda42d16fed1036b1
Syahda Fahreza <syahdafahreza@gmail.com>
2022-11-12 13:18:17 +0700

    Update whatsapp-onlineswitch.js

--- js/whatsapp-onlineswitch.js
+++ js/whatsapp-onlineswitch.js
@@ -1,5 +1,30 @@
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
 const d = new Date();
 let day = weekday[d.getDay()];
-document.getElementById("OnlineStatus").innerHTML = day;
+if (day == "Monday") {
+	if (new Date().getHours() >= 8 && < 16 ) {
+  document.getElementById("OnlineStatus").innerHTML = "Online";}
+}
+else if (day == "Tuesday") {
+	if (new Date().getHours() >= 8 && < 16 ) {
+  document.getElementById("OnlineStatus").innerHTML = "Online";}
+}
+else if (day == "Wednesday") {
+	if (new Date().getHours() >= 8 && < 16 ) {
+  document.getElementById("OnlineStatus").innerHTML = "Online";}
+}
+else if (day == "Thursday") {
+	if (new Date().getHours() >= 8 && < 16 ) {
+  document.getElementById("OnlineStatus").innerHTML = "Online";}
+}
+else if (day == "Friday") {
+	if (new Date().getHours() >= 8 && < 16 ) {
+  document.getElementById("OnlineStatus").innerHTML = "Online";}
+}
+else if (day == "Saturday") {
+	if (new Date().getHours() >= 8 && < 1 ) {
+  document.getElementById("OnlineStatus").innerHTML = "Online";}
+}
+else {
+  document.getElementById("OnlineStatus").innerHTML = "Offline";
+}
